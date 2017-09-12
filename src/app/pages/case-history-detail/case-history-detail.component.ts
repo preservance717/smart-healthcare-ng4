e04318b9886877f3a31a4934eac7612c5938218c
LG <GL717@outlook.com>
2017-09-12 16:26:19 +0800

    generate case history detail page

--- src/app/pages/case-history-detail/case-history-detail.component.ts
+++ src/app/pages/case-history-detail/case-history-detail.component.ts
@@ -6,24 +6,18 @@ import {CornerstoneService} from "../../theme/services/cornerstone.service";
 
 @Component({
   selector: 'case-history-detail',
-  templateUrl: './case-history-detail.component.html'
+  templateUrl: 'case-history-detail.component.html',
+  styleUrls: ['case-history-detail.component.scss']
 })
 
-export class CaseHistoryDetailComponent implements OnInit{
+export class CaseHistoryDetailComponent implements OnInit {
   imageData: any;
 
   constructor(public csS: CornerstoneService) {
-
   }
 
-
   ngOnInit() {
-
-    // for (const i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11 , 12 , 13, 14 ,15 , 16 , 17 , 18]) {
     this.csS.fetchDicomImage(`http://localhost:4200/assets/dicom/im1.dcm`)
-      .subscribe(res =>  this.imageData = res);
-    // }
-
-
+      .subscribe(res => this.imageData = res);
   }
 }
