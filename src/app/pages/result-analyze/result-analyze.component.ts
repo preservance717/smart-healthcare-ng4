7b3914857e529814de982f853ca78b8a6913e042
LG <GL717@outlook.com>
2017-09-12 15:21:20 +0800

    generate result list page

--- src/app/pages/result-analyze/result-analyze.component.ts
+++ src/app/pages/result-analyze/result-analyze.component.ts
@@ -2,9 +2,156 @@ import {Component} from "@angular/core";
 
 @Component({
   selector:'result-analyze',
-  template:`<div>result-analyze</div>`
+  templateUrl:'result-analyze.component.html',
+  styleUrls:['result-analyze.component.scss']
 })
 
 export class ResultAnalyzeComponent{
-
+  columns = [
+    {id: 'id', name: 'ID', prop: 'id'},
+    {id: 'name', name: '姓名', prop: 'name'},
+    {id: 'sex', name: '性别', prop: 'sex'},
+    {id: 'age', name: '接尘工龄', prop: 'age'},
+    {id: 'property', name: '粉尘性质', prop: 'property'},
+    {id: 'telephone', name: '上传医生', prop: 'telephone'},
+    {id: 'time', name: '上传时间', prop: 'time'},
+    {}
+  ];
+  rows = [
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    },
+    {
+      "id": 1,
+      "name": "Ethel Price",
+      "sex": "女",
+      "telephone": "1234345654",
+      "age": "13",
+      "property": "3级",
+      "time": "03/08/17 08:37"
+    }
+  ]
 }
