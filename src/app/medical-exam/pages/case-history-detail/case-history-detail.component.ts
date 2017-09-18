d90cfd5cb90b342de5a641049d8efefdd3800da4
LG <GL717@outlook.com>
2017-09-18 22:56:23 +0800

    modify style

--- src/app/medical-exam/pages/case-history-detail/case-history-detail.component.ts
+++ src/app/medical-exam/pages/case-history-detail/case-history-detail.component.ts
@@ -142,21 +142,63 @@ export class CaseHistoryDetailComponent implements OnInit,AfterViewInit {
             if (this.imageData.imageId) {
               // this.displayImage(this.imageData);
               cornerstone.displayImage(this.fullDcmEle.nativeElement, this.imageData);
+              $('#transferSyntax').text(this.getTransferSyntax());
+              $('#sopClass').text(this.getSopClass());
               $('#samplesPerPixel').text(this.imageData.data.uint16('x00280002'));
               $('#columns').text(this.imageData.data.uint16('x00280011'));
+              $('#photometricInterpretation').text(this.imageData.data.string('x00280004'));
+              $('#numberOfFrames').text(this.imageData.data.string('x00280008'));
+              $('#rows').text(this.imageData.data.uint16('x00280010'));
               $('#pixelSpacing').text(this.imageData.data.string('x00280030'));
+              $('#planarConfiguration').text(this.getPlanarConfiguration());
               $('#bitsAllocated').text(this.imageData.data.uint16('x00280100'));
               $('#bitsStored').text(this.imageData.data.uint16('x00280101'));
               $('#highBit').text(this.imageData.data.uint16('x00280102'));
+              $('#pixelRepresentation').text(this.getPixelRepresentation());
               $('#windowCenter').text(this.imageData.data.string('x00281050'));
               $('#windowWidth').text(this.imageData.data.string('x00281051'));
               $('#rescaleIntercept').text(this.imageData.data.string('x00281052'));
               $('#rescaleSlope').text(this.imageData.data.string('x00281053'));
+              $('#basicOffsetTable').text(this.imageData.data.elements.x7fe00010.basicOffsetTable ? this.imageData.data.elements.x7fe00010.basicOffsetTable.length : '');
+              $('#fragments').text(this.imageData.data.elements.x7fe00010.fragments ? this.imageData.data.elements.x7fe00010.fragments.length : '');
+              $('#minStoredPixelValue').text(this.imageData.minPixelValue);
+              $('#maxStoredPixelValue').text(this.imageData.maxPixelValue);
+              // var end = new Date().getTime();
+              // var time = end - start;
+              // $('#totalTime').text(time + "ms");
+              $('#loadTime').text(this.imageData.loadTimeInMS + "ms");
+              $('#decodeTime').text(this.imageData.decodeTimeInMS + "ms");
             }
           }
         });
     }, 0)
   }
+
+  getTransferSyntax() {
+  var value = this.imageData.data.string('x00020010');
+  return value;
+}
+
+  getSopClass() {
+  var value = this.imageData.data.string('x00080016');
+  return value;
+}
+
+  getPixelRepresentation() {
+  var value = this.imageData.data.uint16('x00280103');
+  if(value === undefined) {
+    return;
+  }
+  return value + (value === 0 ? ' (unsigned)' : ' (signed)');
+}
+
+  getPlanarConfiguration() {
+  var value = this.imageData.data.uint16('x00280006');
+  if(value === undefined) {
+    return;
+  }
+  return value + (value === 0 ? ' (pixel)' : ' (plane)');
+}
 
   backDetail() {
     this.fullScreenBtn = false;
