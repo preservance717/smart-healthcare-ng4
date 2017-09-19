d94acf919dbf0214b3ee95a78f5a3769730271be
LG <GL717@outlook.com>
2017-09-19 22:19:47 +0800

    call interface of patient detail

--- src/app/medical-exam/pages/medical-people-detail/medical-people-detail.component.ts
+++ src/app/medical-exam/pages/medical-people-detail/medical-people-detail.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 import {CornerstoneService} from "../../../theme/services/cornerstone.service";
+import {MedicalPeopleDetailService} from "./medical-people-detail.service";
 declare const cornerstone;
 @Component({
   selector: 'app-medical-people-detail',
   templateUrl: './medical-people-detail.component.html',
-  styleUrls: ['medical-people-detail.component.scss']
+  styleUrls: ['medical-people-detail.component.scss'],
+  providers:[MedicalPeopleDetailService]
 })
 export class MedicalPeopleDetailComponent implements OnInit {
   imageData: any;
@@ -13,9 +15,20 @@ export class MedicalPeopleDetailComponent implements OnInit {
   demoList = [1,2];
   dcmElement:any;
 
-  constructor(public csS: CornerstoneService,public elementRef:ElementRef) { }
+  patientInfo:any;
+
+  constructor(public csS: CornerstoneService,public elementRef:ElementRef,public _service:MedicalPeopleDetailService) { }
 
   ngOnInit() {
+    this.reviewPatientDetail();
+  }
+
+  reviewPatientDetail(){
+    let patientId = sessionStorage.getItem("patientId");
+    this._service.reviewPatientDetail(patientId)
+      .then(res=>{
+        this.patientInfo = res.data;
+      })
   }
 
   openAccordion(i){
---
+++ src/app/medical-exam/pages/medical-people-detail/medical-people-detail.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from "@angular/core";
+import {Http, RequestOptions, Headers} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+import {GlobalState} from "../../../global.state";
+
+@Injectable()
+export class MedicalPeopleDetailService {
+  patientDetailUrl: string = '/selectOnePatient';
+
+  constructor(private http: Http, private GlobalState: GlobalState) {
+    this.patientDetailUrl = this.GlobalState.MEBaseUrl + this.patientDetailUrl;
+  }
+
+  reviewPatientDetail(patientId:string):Promise<any> {
+    this.patientDetailUrl = `${this.patientDetailUrl}\/${patientId}`;
+    return this.http.get(this.patientDetailUrl).toPromise()
+      .then(response => response.json())
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    // console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+}
