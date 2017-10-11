fa54601d68e17935582ec4a7ecf4c2c7a8f3f535
LG <GL717@outlook.com>
2017-10-11 14:27:29 +0800

    add full screen in medical people detail

--- src/app/medical-exam/pages/medical-people-detail/medical-people-detail.component.ts
+++ src/app/medical-exam/pages/medical-people-detail/medical-people-detail.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 import {CornerstoneService} from "../../../theme/services/cornerstone.service";
 import {MedicalPeopleDetailService} from "./medical-people-detail.service";
+import * as $ from "jquery";
 declare const cornerstone;
+declare const cornerstoneTools;
+
 @Component({
   selector: 'app-medical-people-detail',
   templateUrl: './medical-people-detail.component.html',
@@ -9,14 +12,20 @@ declare const cornerstone;
   providers:[MedicalPeopleDetailService]
 })
 export class MedicalPeopleDetailComponent implements OnInit {
+  @ViewChild('fullDcmEle') fullDcmEle: ElementRef;
+
   imageData: any;
   imageList = [];
 
   dcmElement:any;
+  fullDcmElement:any;
 
   patientInfo:any;
+  fullScreenBtn: boolean = false;
+  index:number = 0;
 
-  constructor(public csS: CornerstoneService,public elementRef:ElementRef,public _service:MedicalPeopleDetailService) { }
+  constructor(public csS: CornerstoneService,public elementRef:ElementRef,public _service:MedicalPeopleDetailService) {
+  }
 
   ngOnInit() {
     this.reviewPatientDetail();
@@ -31,6 +40,7 @@ export class MedicalPeopleDetailComponent implements OnInit {
   }
 
   openAccordion(i){
+    this.index = i;
     this.dcmElement = this.elementRef.nativeElement.querySelector("#dicomImage"+i);
     setTimeout(() => {
       cornerstone.enable(this.dcmElement);
@@ -51,4 +61,65 @@ export class MedicalPeopleDetailComponent implements OnInit {
   displayImage(image) {
     cornerstone.displayImage(this.dcmElement, image);
   }
+
+  reviewFullScreen() {
+    this.fullScreenBtn = true;
+    // this.fullDcmElement = this.elementRef.nativeElement.querySelector("#fullDicomImage");
+    // console.log("this.fullDcmElement",this.fullDcmElement);
+    setTimeout(() => {
+      cornerstone.enable(this.fullDcmEle.nativeElement);
+
+      this.csS.fetchDicomImage(`/TM/files/${this.patientInfo.xrayTaskDTOList[this.index].fileName}`)
+        .subscribe(res => {
+          this.imageData = res;
+          if (this.imageData) {
+
+            if (!this.imageList.filter(img => img.imageId === this.imageData.imageId).length) {
+              this.imageList.push(this.imageData);
+            }
+
+            if (this.imageData.imageId) {
+              this.activeDcm(this.fullDcmEle.nativeElement, this.imageData);
+              this.dcmDescripe();
+            }
+          }
+        });
+    }, 0)
+  }
+  dcmDescripe(){
+    $('#columns').text(this.imageData.data.uint16('x00280011'));
+    $('#rows').text(this.imageData.data.uint16('x00280010'));
+  }
+
+  activeDcm(ele, imageData){
+    cornerstone.displayImage(ele, imageData);
+    cornerstoneTools.mouseInput.enable(ele);
+    cornerstoneTools.mouseWheelInput.enable(ele);
+
+    // Enable all tools we want to use with this ele
+    cornerstoneTools.wwwc.activate(ele, 1); // ww/wc is the default tool for left mouse button
+    cornerstoneTools.pan.activate(ele, 2); // pan is the default tool for middle mouse button
+    cornerstoneTools.zoom.activate(ele, 4); // zoom is the default tool for right mouse button
+    cornerstoneTools.zoomWheel.activate(ele); // zoom is the default tool for middle mouse wheel
+  }
+
+  backDetail() {
+    this.fullScreenBtn = false;
+    setTimeout(() => {
+      // this.initImage(this.patientInfo.xrayTaskDTOList[this.index].fileName);
+      cornerstone.enable(this.dcmElement);
+
+      this.csS.fetchDicomImage(`/TM/files/${this.patientInfo.xrayTaskDTOList[this.index].fileName}`)
+        .subscribe(res => {
+          this.imageData = res;
+          if (this.imageData) {
+            this.displayImage(this.imageData);
+            if (!this.imageList.filter(img => img.imageId === this.imageData.imageId).length) {
+              this.imageList.push(this.imageData);
+            }
+          }
+        });
+    }, 0);
+  }
+
 }
